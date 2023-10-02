f0212f7bea92cfd3c16e8f5beab7c4cbbd545a0f
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2023-10-02 17:13:31 +0100

    Add selection code to determine winner

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -6,7 +6,6 @@ public class RockPaperScissors {
         //Computer selects move
         String[] computerMoves = {"r", "p", "s"};
         String computerSelection = computerMoves[new Random().nextInt(computerMoves.length)];
-        
         //User selects move
         Scanner scanner = new Scanner(System.in);
         String userSelection;
@@ -20,6 +19,38 @@ public class RockPaperScissors {
             System.out.println(userSelection + "is not a valid selection");
         }
 
+        //Compare the moves to determine result and print result
+        //Draw Condition
+        if(userSelection.equals(computerSelection)){
+            System.out.println("It's a draw!");
+        }
+        //Player Chooses "r"
+        else if (userSelection.equals("r")){
+            if(computerSelection.equals("p")){
+                System.out.println("Computer wins! Paper beats Rock");
+            }
+            if(computerSelection.equals("s")){
+                System.out.println("User wins! Rock beats Scissors");
+            }
+        }
+        //Player Chooses "p"
+        else if (userSelection.equals("p")){
+            if(computerSelection.equals("s")){
+                System.out.println("Computer wins! Scissors beats Paper");
+            }
+            if(computerSelection.equals("r")){
+                System.out.println("User wins! Paper beats Rock");
+            }
+        }
+        //Player Chooses "s"
+        else if (userSelection.equals("s")){
+            if(computerSelection.equals("r")){
+                System.out.println("Computer wins! Rock beats Scissors");
+            }
+            if(computerSelection.equals("p")){
+                System.out.println("User wins! Scissors beats Paper");
+            }
+        }
         scanner.close();
     }
 }
