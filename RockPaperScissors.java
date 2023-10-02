55fa2dd168b0c12997061416364bd9104a3456b1
Arturo Salazar <asalazar@hawk.iit.edu>
2023-10-02 10:45:11 -0500

    Add functionality for computer to select move

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,5 +1,9 @@
+import java.util.Random;
+
 public class RockPaperScissors {
     public static void main (String[] args){
-
+        //Computer selects move
+        String[] computerMoves = {"r", "p", "s"};
+        String computerSelection = computerMoves[new Random().nextInt(computerMoves.length)];
     }
 }
